ba90f4b5e6164e62fe3fe3efe053e5c869ca9a6a
admin <gmgd055@gmail.com>
2026-07-28 11:14:25 +0900

    first commit

---
+++ src/day01/Exam1.java
@@ -0,0 +1,16 @@
+package day01;
+
+/**
+ * Exam
+ */
+public class Exam1 {
+    int a = 10;
+    // System.out.printin("안녕자바") //오류발생
+
+    public static void main(String[] args) {
+        int b = 10;
+        System.out.println(
+             "안녕자바");
+    //[3] ;(세미콜론) : 코드의 한문장이 끝났다는 뜻
+    }
+}
